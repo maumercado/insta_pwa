86e420f8994952df46b40dde1583661e8c518045
Mauricio Mercado <mau@maumercado.com>
2018-01-23 21:05:55 -0800

    Add support to send notifications through firebase functions

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,7 @@
 var functions = require("firebase-functions");
 var admin = require("firebase-admin");
 var cors = require("cors")({ origin: true });
+var webpush = require("web-push");
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 
@@ -23,6 +24,35 @@ exports.storePostData = functions.https.onRequest(function(request, response) {
                 image: request.body.image
             })
             .then(function() {
+                webpush.setVapidDetails(
+                    "mailto: info@maumercado.com",
+                    "BB3NdO1ImBYAQ-G1M4hcy_qIZ6N-VNS-MS7DoNQVCAN_FrZDx6LVcIOshlI09lZmWGuNvQrQ4JlPy44ve_i0lXI",
+                    "LFR9XVUtKJ6HhF42p5Yt1jvUUCF60qea93NiT78uquM"
+                );
+                return admin.database.ref("subscription").once("value");
+            })
+            .then(function(subscriptions) {
+                subscriptions.forEach(function(sub) {
+                    var pushConfig = {
+                        endpoint: sub.val().endpoint,
+                        keys: {
+                            auth: sub.val().keys.auth,
+                            p256df: sub.val().keys.p256dh
+                        }
+                    };
+                    webpush
+                        .sendNotification(
+                            pushConfig,
+                            JSON.stringify({
+                                title: "New Post",
+                                content: "New Post added!",
+                                openUrl: "/help"
+                            })
+                        )
+                        .catch(function(err) {
+                            console.error(err);
+                        });
+                });
                 response
                     .status(201)
                     .json({ message: "Data stored", id: request.body.id });
